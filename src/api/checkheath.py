f050239c078b29ad031e5ae4bd5e4a1a1d9f1f0a
DESKTOP-KQA2RP4\ThinkPad <chinhpham1609@gmail.com>
2026-04-04 05:43:49 +0100

    check kafka

--- src/api/checkheath.py
+++ src/api/checkheath.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter, BackgroundTasks, HTTPException
 from src.core.config import settings
 from src.core.logging import logger
+from confluent_kafka.admin import AdminClient
+from confluent_kafka import Producer
+
+KAFKA_BOOTSTRAP_SERVERS = f"{settings.KAFKA_BROKER_HOST}:{settings.KAFKA_BROKER_PORT}"
+# --- Kafka clients -------------------------------------------------
+admin = AdminClient({"bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS})
+producer = Producer({"bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS})
 
 router = APIRouter(prefix="/api/v1/check", tags=["Check"])
 
@@ -9,3 +16,30 @@ async def check_health():
     return {
         "status" : "OK"
     }
+
+@router.get("/health/kafka")
+async def check_kafka():
+    try:
+        # gọi metadata từ Kafka
+        metadata = admin.list_topics(timeout=3)
+
+        # nếu có broker là OK
+        brokers = metadata.brokers
+
+        if not brokers:
+            return {
+                "status": "DOWN",
+                "detail": "No brokers found"
+            }
+
+        return {
+            "status": "UP",
+            "brokers": len(brokers),
+            "topics": len(metadata.topics)
+        }
+
+    except Exception as e:
+        return {
+            "status": "DOWN",
+            "error": str(e)
+        }
