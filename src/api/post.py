bb24980e1faa463ec6a12d4224a0e4c2ab017eb9
DESKTOP-KQA2RP4\ThinkPad <chinhpham1609@gmail.com>
2026-04-10 04:44:02 +0100

    pis

--- src/api/post.py
+++ src/api/post.py
@@ -54,7 +54,7 @@ async def insert_posts_unclassified(request: dict, background_tasks: BackgroundT
         # background_tasks.add_task(send_to_kafka, topic, cleaned_data)
 
         if len(data) > 0:
-            background_tasks.add_task(track_bot.track_bot, data)
+            # background_tasks.add_task(track_bot.track_bot, data)
             background_tasks.add_task(send_to_kafka, topic, data)
 
         return {"status": "OK", "detail": f"Sent to topic '{topic}'"}
